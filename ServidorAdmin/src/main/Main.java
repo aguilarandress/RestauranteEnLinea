bea93cf1397cdc04d0105997ccd28e6776b41754
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2019-11-19 14:09:35 -0600

    Servidor listo

--- ServidorAdmin/src/main/Main.java
+++ ServidorAdmin/src/main/Main.java
@@ -1,9 +1,13 @@
 package main;
 
+import server.TCPServer;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		// Inicializar servidor
+		TCPServer mainServer = new TCPServer();
+		new Thread(mainServer).start();
 	}
 
 }
--- ServidorAdmin/src/server/TCPServer.java
+++ ServidorAdmin/src/server/TCPServer.java
@@ -3,6 +3,7 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -10,19 +11,25 @@ public class TCPServer implements Runnable {
 
     private final int PORT = 9090;
     private ExecutorService pool = Executors.newFixedThreadPool(15);
+    private ArrayList<ClientHandler> clients = new ArrayList<>();
 
     @Override
     public void run() {
         try {
             // Iniciar servidor
             ServerSocket listener = new ServerSocket(this.PORT);
+            System.out.println("Servidor iniciado en el puerto: " + this.PORT);
             // Esperar conexiones
             while (true) {
                 System.out.println("[SERVER] Esperando conexiones...");
                 // Crear conexion
                 Socket cliente = listener.accept();
                 System.out.println("[SERVER] Un cliente se ha conectado");
-
+                // Spawnear un client handler para la conexion
+                ClientHandler clientHandler = new ClientHandler(cliente);
+                clients.add(clientHandler);
+                // Agregar al thread pool
+                pool.execute(clientHandler);
             }
         } catch (IOException e) {
             e.printStackTrace();
