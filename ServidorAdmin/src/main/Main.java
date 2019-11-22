26aa0c77a56b07e58bf326f69ed0acbf62de93a3
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2019-11-21 20:24:31 -0600

    Montos completos

--- ServidorAdmin/src/main/Main.java
+++ ServidorAdmin/src/main/Main.java
@@ -11,7 +11,6 @@ import models.catalogo.Catalogo;
 public class Main {
 
 	public static void main(String[] args) {
-
     try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
