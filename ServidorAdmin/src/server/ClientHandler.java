20156fd22ca902d6337eeeb4758ed8ebe38a2a8a
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2019-11-23 17:40:18 -0600

    Recibir peticionde alimentos

--- ServidorAdmin/src/server/ClientHandler.java
+++ ServidorAdmin/src/server/ClientHandler.java
@@ -3,6 +3,7 @@ package server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 public class ClientHandler implements Runnable {
 
     private Socket cliente;
-    private BufferedReader in;
+    private ObjectInputStream inputObject;
     private TCPServer server;
     public PrintWriter out;
 
@@ -23,7 +24,7 @@ public class ClientHandler implements Runnable {
         this.cliente = clientSocket;
         this.server = server;
         // Crear streams de input y output
-        in = new BufferedReader(new InputStreamReader(this.cliente.getInputStream()));
+        this.inputObject = new ObjectInputStream(this.cliente.getInputStream());
         out = new PrintWriter(cliente.getOutputStream(), true);
     }
 
@@ -31,34 +32,53 @@ public class ClientHandler implements Runnable {
     public void run() {
         try {
             while (true) {
+            	Object inputRecibido = (Object) this.inputObject.readObject();
+            	// Es un mensaje tipo string
+            	if (inputRecibido instanceof String) {
+            		// El cliente desea salir
+            		if (inputRecibido.equals("quit")) {
+            			this.server.getController().agregarActividadDeConexion("[SERVER] Cliente desconectado...");
+            			cliente.close();
+            			break;
+            		}
+            		else if (inputRecibido.equals("alimentos")) {
+            			
+            		}
+            	}
+            	
+            	
                 // Obtener request del cliente
-                String request = in.readLine();
-                if (request == null) break;
-                // Obtener tokens
-                String[] message = request.split(" ");
-                // Revisar si se desea terminar la conexion
-                if (request.equals("quit")) {
-                	// Desconectar cliente del servidor
-                	this.server.getController().agregarActividadDeConexion("[SERVER] Cliente desconectado...");
-                    cliente.close();
-                    break;
-                }
-                else if (message[0].equals("send")) {
-                	this.server.sendToController(message[1]);
-                	this.out.println("client HelloWorld");
-                }
-                else {
-                    System.out.println("[SERVER] Se recibio: " + request);
-                    // Responder al cliente
-                    out.println("El servidor recibio: " + request);
-                }
+//                String request = in.readLine();
+//                if (request == null) break;
+//                // Obtener tokens
+//                String[] message = request.split(" ");
+//                // Revisar si se desea terminar la conexion
+//                if (request.equals("quit")) {
+//                	// Desconectar cliente del servidor
+//                	this.server.getController().agregarActividadDeConexion("[SERVER] Cliente desconectado...");
+//                    cliente.close();
+//                    break;
+//                }
+//                else if (message[0].equals("send")) {
+//                	this.server.sendToController(message[1]);
+//                	this.out.println("client HelloWorld");
+//                }
+//                else {
+//                    System.out.println("[SERVER] Se recibio: " + request);
+//                    // Responder al cliente
+//                    out.println("El servidor recibio: " + request);
+//                }
             }
         } catch (IOException e) {
             e.getStackTrace();
-        } finally {
+        } catch (ClassNotFoundException e) {
+			System.out.println("No se encontro la clase...");
+			e.printStackTrace();
+		} finally {
             out.close();
             try {
-                in.close();
+                //in.close();
+                this.inputObject.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
